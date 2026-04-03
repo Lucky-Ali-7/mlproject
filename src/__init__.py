a9980db40969afa67592165575a609bf90f0e032
John DoeLucky <beinglucky123@gmail.com>
2026-04-03 17:35:03 +0530

    Fix logging setup and data ingestion imports

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+from .pipeline import logger, exception
+from .pipeline.exception import CustomException
--- src/pipeline/exception.py
+++ src/pipeline/exception.py
@@ -1,5 +1,5 @@
 import sys
-from src.logger import logging
+from .logger import logging
 
 # Configure basic logging to console if not already configured
 if not logging.getLogger().hasHandlers():
